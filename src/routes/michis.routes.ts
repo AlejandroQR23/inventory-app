44cdf312111f05d1cb64bc63a6dac6f0a139dfb7
Alejandro QR <alejandro.quijano@comunidad.unam.mx>
2022-06-25 14:06:03 -0500

    created routes for michi model

--- src/routes/michis.routes.ts
+++ src/routes/michis.routes.ts
@@ -1,15 +1,18 @@
 import { Router } from 'express';
 import passport from 'passport';
+import { createMichi, deleteMichi, getMichiById, getMichis, updateMichi } from '../controllers/michis.controller';
 
 const router = Router();
 
-router.get('/michis', (req, res) => {
-  res.send('michis');
-});
+router.get('/', getMichis);
 
-// authenticated route
-router.post('/michis', passport.authenticate('jwt', { session: false }), (req, res) => {
-  res.send('done');
-});
+router.get('/:id', getMichiById);
+
+// * authenticated routes
+router.post('/', passport.authenticate('jwt', { session: false }), createMichi);
+
+router.put('/:id', passport.authenticate('jwt', { session: false }), updateMichi);
+
+router.delete('/:id', passport.authenticate('jwt', { session: false }), deleteMichi);
 
 export default router;
